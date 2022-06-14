840779eddd43f0f12c2447cc760a82e3df59a121
Mateus Mendes <60234944+VOTRON157@users.noreply.github.com>
2022-06-14 19:54:08 -0300

    Create 2374 - Pneu.cs

---
+++ 2374 - Pneu.cs
@@ -0,0 +1,10 @@
+using System;
+// using System.Globalization;
+// using System.Threading;
+class Script
+{
+    static private void Main()
+    {
+        Console.WriteLine(Convert.ToInt32(Console.ReadLine()) - Convert.ToInt32(Console.ReadLine()));
+    }
+}
